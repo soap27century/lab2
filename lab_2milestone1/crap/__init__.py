243948f7a6ffc0f7eaa787894796a4f78ba19e34
soap27century <48539706+soap27century@users.noreply.github.com>
2019-12-09 18:55:41 -0500

    Update __init__.py

--- lab_2milestone1/crap/__init__.py
+++ lab_2milestone1/crap/__init__.py
@@ -1,27 +1,14 @@
 import playground,sys,os
 from playground.network.common import StackingProtocolFactory, StackingProtocol, StackingTransport
-# sys.path.insert(1,'../crap/')
-# from .protocol import CrapClient, CrapServer
 from .protocol import CrapClient, CrapServer
 sys.path.insert(1,'../crap/')
-# print("director",os.getcwd())
 from ..poop.protocol import PoopClient, PoopServer
 
 CrapClientFactory = StackingProtocolFactory.CreateFactoryType(PoopClient,CrapClient)
 CrapServerFactory = StackingProtocolFactory.CreateFactoryType(PoopServer,CrapServer)
 
-
-# CrapClientFactory = StackingProtocolFactory.CreateFactoryType(PoopClient)
-# CrapServerFactory = StackingProtocolFactory.CreateFactoryType(PoopServer)
-# CrapClientFactory = StackingProtocolFactory.CreateFactoryType(CrapClient)
-# CrapServerFactory = StackingProtocolFactory.CreateFactoryType(CrapServer)
-
 CrapConnector = playground.Connector(protocolStack=(
     CrapClientFactory(),
     CrapServerFactory()))
-
-# connector = playground.Connector(
-#     protocolStack=(CrapClientFactory(), CrapServerFactory())
-# )
 
 playground.setConnector('crap', CrapConnector)
